65679315172a7570c96bbc2440d4966080ae6d18
Skin Tang <tsjtux@gmail.com>
2018-01-28 11:21:17 +0000

    add more columns

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -15,12 +15,35 @@ class UserController < ApplicationController
         reposUrl = baseurl + "users/" + username + "/repos"
         
         repos = HTTParty.get(reposUrl).parsed_response
-        langs = {}
         User.where(:login => username).destroy_all
-         
+        reposInfo = []
+        repos.each do |repo|
+            repoInfo = {}
+            repoInfo[:repoName] = repo["name"]
+            repoInfo[:lang] = repo["language"]
+            repoInfo[:created_at] = repo["created_at"]
+            repoInfo[:updated_at] = repo["updated_at"]
+            repoInfo[:commits_url] = repo["commits_url"]
+            reposInfo.push(repoInfo)
+        end
+        langRes = getLangRes(reposInfo)
+        commitsRes = countCommits(reposInfo, username)
         user = Octokit.user username
         
         user_params = {}
+        user_params["commits_num"] = commitsRes["commits_num"]
+        user_params["big_repo_commits_num"] = commitsRes["big_repo_commits_num"]
+        user_params["big_repo"] = commitsRes["big_repo"]
+        user_params["late_commit_time"] = commitsRes["late_commit_time"]
+        user_params["late_commit_repo"] = commitsRes["late_commit_repo"]
+        user_params["most_commits_time"] = commitsRes["most_commits_time"]
+        user_params["most_commits_num"] = commitsRes["most_commits_num"]
+        user_params[:lang1] = (langRes.length > 0) ? langRes[0][0] : nil
+        user_params[:lang1num] = (langRes.length > 0) ? langRes[0][1] : nil
+        user_params[:lang2] = (langRes.length > 1) ? langRes[1][0] : nil
+        user_params[:lang2num] = (langRes.length > 1) ? langRes[1][1] : nil
+        user_params[:lang3] = (langRes.length > 2) ? langRes[2][0] : nil
+        user_params[:lang3num] = (langRes.length > 2) ? langRes[2][1] : nil
         user_params[:avatar_url] = user[:avatar_url]
         user_params[:html_url] = user[:html_url]
         user_params[:name] = user[:name]
@@ -30,16 +53,96 @@ class UserController < ApplicationController
         user_params[:login] = user[:login]
         user_params[:createat] = user[:created_at]
         user_params[:updateat] = user[:updated_at]
+
         User.create(user_params)
         #puts params[:username]
-        redirect_to :controller => "user", :action=>"show", :username => data["login"]
+        redirect_to :controller => "user", :action=>"show", :username => username
     end
     
     def show
         @username = params[:username]
         @image = User.find_by_login(@username).avatar_url
+        @user = User.find_by_login(@username)
         render "show"
     end
     
+    def getLangRes(reposInfo)
+        langs = {}
+        reposInfo.each do |repo|
+            if !langs.key?(repo[:lang])
+                langs[repo[:lang]] = 0
+            end
+            langs[repo[:lang]] += 1
+        end
+        langs.sort_by{|_key, value| value}
+        nLang = (langs.length < 3) ? langs.length : 3
+        langRes = []
+        langs.each do |lang|
+            if nLang == 0
+                break
+            end
+            langRes.push(lang)
+            nLang -= 1
+        end
+        return langRes
+    end
     
+    def countCommits(reposInfo, username)
+        commits_num = 0
+        big_repo_commits_num = 0
+        big_repo = nil
+        late_commit_time = 0
+        late_commit_repo = nil
+        most_commits_time = nil
+        most_commits_num = 0
+      
+        late_commit_ptime = 0
+        most_commits_hash = {}
+        baseurl = "http://api.github.com/repos/" + username
+        reposInfo.each do |repo|
+            repoName = repo[:repoName]
+            commitsUrl = baseurl + "/" + repoName + "/commits"
+            commits = HTTParty.get(commitsUrl).parsed_response
+            commitsNum = commits.length
+            commits_num += commitsNum
+            if commitsNum > big_repo_commits_num
+                big_repo_commits_num = commitsNum
+                big_repo = repoName
+            end
+            commits.each do |commit|
+                datetime = commit["commit"]["author"]["date"]
+                #"2017-09-13T19:39:51Z"
+                date = datetime[0, 10]
+                time = datetime[11, 8]
+                if !most_commits_hash.key?(date)
+                    most_commits_hash[date] = 0
+                end
+                most_commits_hash[date] += 1
+                if most_commits_hash[date] > most_commits_num
+                    most_commits_num = most_commits_hash[date]
+                    most_commits_time = datetime
+                end
+                hour = time[0, 2].to_i
+                hour = (hour > 0 && hour < 5) ? hour + 24 : hour
+                min = time[3, 2].to_i
+                sec = time[6, 2].to_i
+                ptime = hour * 3600 + min * 60 + sec
+                if ptime > late_commit_ptime
+                    late_commit_ptime = ptime
+                    late_commit_time = datetime
+                    late_commit_repo = repoName
+                end
+            end
+        end
+        res = {}
+        res["commits_num"] = commits_num
+        res["big_repo_commits_num"] = big_repo_commits_num
+        res["big_repo"] = big_repo
+        res["late_commit_time"] = late_commit_time
+        res["late_commit_repo"] = late_commit_repo
+        res["most_commits_time"] = most_commits_time
+        res["most_commits_num"] = most_commits_num
+        return res
+    end
+
 end
